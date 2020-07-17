b8e5b19ca5a5dee2e0f356054a7012b55f5e8afc
Uriel281198 <urielarriaga.1998@gmail.com>
2020-07-16 22:22:37 -0500

    Upload repo

---
+++ src/Components/details/page.js
@@ -0,0 +1,14 @@
+
+import React,{Component} from 'react';
+
+
+
+class Page extends Component{
+    render(){
+        return(
+            <h1>Page</h1>
+        )        
+    }
+}
+
+export default Page;
---
+++ src/Components/results/page.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+class Page extends Component {
+    render(){
+        return(
+            <h1>hello</h1>
+        )
+    }
+
+}
+
+export default Page;
